fbe97472c6e3d28905b231139b9504ebada61a31
RotenBart <beard.voloshin@gmail.com>
2019-10-26 00:09:57 +0300

    second layout + grid

--- app/src/main/java/android/pvt/softtecotest/ui/MainActivity.kt
+++ app/src/main/java/android/pvt/softtecotest/ui/MainActivity.kt
@@ -7,15 +7,20 @@ import android.pvt.softtecotest.mvvm.MVVMState
 import android.pvt.softtecotest.mvvm.ViewModelPosts
 import android.pvt.softtecotest.recycler.PostGridAdapter
 import android.util.Log
+import android.widget.GridView
+import android.widget.HorizontalScrollView
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : FragmentActivity(), PostGridAdapter.OnClickListener {
 
     var posts: MutableList<Post> = mutableListOf()
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: PostGridAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,20 +31,31 @@ class MainActivity : FragmentActivity(), PostGridAdapter.OnClickListener {
             when (it) {
                 is MVVMState.Data -> {
                     Log.e("QQQ1", it.postList.toString())
-                    posts.addAll(it.postList)
+                    //posts.addAll(it.postList)
+                    recyclerView.adapter = PostGridAdapter(it.postList, this)
                 }
                 is MVVMState.Error -> {
+                    Log.e("QQQEEE", "ERROR")
                 }
             }
         })
 
-        Log.e("QQQ", posts.toString())
-        val recyclerView = findViewById<RecyclerView>(R.id.postRecyclerView)
+        recyclerView = findViewById<RecyclerView>(R.id.postRecyclerView)
         recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val gridLayout = GridLayoutManager(this, 6, GridLayoutManager.HORIZONTAL, false )
+            gridLayout.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+
+            override fun getSpanSize(position: Int): Int {
+                return 3
+            }
+        }
+
+        recyclerView.layoutManager = gridLayout
+
+
         recyclerView.isNestedScrollingEnabled = false
-        val adapter = PostGridAdapter(posts, this)
-        recyclerView.adapter = adapter
+        //recyclerView.adapter = adapter
 
 
     }
